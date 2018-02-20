d0f09a27378f776f03d7325cd7fce2dd96190b45
jeyj0 <jeyj0.plus@gmail.com>
2018-02-20 18:06:12 +0100

    Moved neural net contents to separate file

---
+++ src/neuralNet.go
@@ -0,0 +1,73 @@
+package main
+
+type Neuron struct {
+	value         float64
+	bias          float64
+	biasWeight    float64
+	inConnections []*Connection
+	isCalculated  bool
+}
+
+type Connection struct {
+	inNeuron *Neuron
+	weight   float64
+}
+
+type Net struct {
+	inputNeurons  []*Neuron
+	outputNeurons []*Neuron
+}
+
+func (n *Neuron) calculateValue() {
+	if !n.isCalculated {
+		newValue := 0.0
+		weightSum := 0.0
+
+		if n.inConnections == nil {
+			newValue = n.value
+			weightSum = 1.0
+		} else {
+			for _, connection := range n.inConnections {
+				newValue += connection.inNeuron.value * connection.weight
+				weightSum += connection.weight
+			}
+		}
+		newValue += n.bias * n.biasWeight
+		newValue /= weightSum + n.biasWeight
+
+		n.value = newValue
+
+		n.isCalculated = true
+	}
+}
+
+func (n *Neuron) calculateValueRecursive() {
+	if n.inConnections != nil {
+		for _, connection := range n.inConnections {
+			connection.inNeuron.calculateValueRecursive()
+		}
+	}
+
+	n.calculateValue()
+}
+
+func (net *Net) reset() {
+	resetRecursive(net.outputNeurons)
+}
+
+func resetRecursive(neurons []*Neuron) {
+	for _, neuron := range neurons {
+		neuron.isCalculated = false
+		nextNeurons := []*Neuron{}
+		for _, connection := range neuron.inConnections {
+			nextNeurons = append(nextNeurons, connection.inNeuron)
+		}
+		resetRecursive(nextNeurons)
+	}
+}
+
+func (net *Net) calculateOutputs() {
+	for _, outputNeuron := range net.outputNeurons {
+		outputNeuron.calculateValueRecursive()
+	}
+}
