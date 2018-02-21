e39cc2bb0c948d7e90fa6c5c2d6993bc52e16585
jeyj0 <jeyj0.plus@gmail.com>
2018-02-21 20:34:39 +0100

    (green) combine seeds

--- src/nnTraining.go
+++ src/nnTraining.go
@@ -1,5 +1,15 @@
 package main
 
+import "math/rand"
+
 func combineSeeds(seed1, seed2 []float64) []float64 {
-	return nil
+	newSeed := []float64{}
+	for i, seed1Val := range seed1 {
+		if rand.Intn(2) == 0 {
+			newSeed = append(newSeed, seed1Val)
+		} else {
+			newSeed = append(newSeed, seed2[i])
+		}
+	}
+	return newSeed
 }
--- src/nnTraining_test.go
+++ src/nnTraining_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,7 @@ import (
 
 func TestCombineSeeds(t *testing.T) {
 	// given
+	rand.Seed(1)
 	seed1 := []float64{1, 2, 3, 4, 5, 6, 7, 8}
 	seed2 := []float64{8, 7, 6, 5, 4, 3, 2, 1}
 
@@ -15,5 +17,5 @@ func TestCombineSeeds(t *testing.T) {
 	seedsCombined := combineSeeds(seed1, seed2)
 
 	// then
-	assert.Equal(t, []float64{1, 2, 3, 5, 4, 3, 2, 1}, seedsCombined)
+	assert.Equal(t, []float64{8, 7, 6, 5, 4, 6, 2, 8}, seedsCombined)
 }
