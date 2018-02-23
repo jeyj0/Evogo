ccb02cdf1d03bc0e5efcebe3194da4fafcf47574
Jannis Jorre <jannis.jorre@bonprix.net>
2018-02-23 12:31:43 +0100

    Moved neuralnet tests into test package

--- neuralnet/net_test.go
+++ neuralnet/net_test.go
@@ -1,28 +1,30 @@
-package neuralnet
+package neuralnet_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	nn "github.com/jeyj0/Evogo/neuralnet"
 )
 
 func TestCalculateOutputs(t *testing.T) {
 	// given
-	input1 := Neuron{Value: 1}
-	input2 := Neuron{Value: 2}
+	input1 := nn.Neuron{Value: 1}
+	input2 := nn.Neuron{Value: 2}
 
-	out1Connections := []*Connection{
-		&Connection{InNeuron: &input1, Weight: 3},
-		&Connection{InNeuron: &input2, Weight: 5}}
+	out1Connections := []*nn.Connection{
+		&nn.Connection{InNeuron: &input1, Weight: 3},
+		&nn.Connection{InNeuron: &input2, Weight: 5}}
 
-	out2Connections := []*Connection{
-		&Connection{InNeuron: &input1, Weight: 4},
-		&Connection{InNeuron: &input2, Weight: 6}}
+	out2Connections := []*nn.Connection{
+		&nn.Connection{InNeuron: &input1, Weight: 4},
+		&nn.Connection{InNeuron: &input2, Weight: 6}}
 
-	output1 := Neuron{Bias: 7, BiasWeight: 8, InConnections: out1Connections}
-	output2 := Neuron{Bias: 9, BiasWeight: 10, InConnections: out2Connections}
+	output1 := nn.Neuron{Bias: 7, BiasWeight: 8, InConnections: out1Connections}
+	output2 := nn.Neuron{Bias: 9, BiasWeight: 10, InConnections: out2Connections}
 
-	net := Net{InputNeurons: []*Neuron{&input1, &input2}, OutputNeurons: []*Neuron{&output1, &output2}}
+	net := nn.Net{InputNeurons: []*nn.Neuron{&input1, &input2}, OutputNeurons: []*nn.Neuron{&output1, &output2}}
 
 	// when
 	net.CalculateOutputs()
@@ -34,20 +36,20 @@ func TestCalculateOutputs(t *testing.T) {
 
 func TestCalculateOutputsWithInputBias(t *testing.T) {
 	// given
-	input1 := Neuron{Value: 1, Bias: 2, BiasWeight: 1} // 1.5
-	input2 := Neuron{Value: 2, Bias: 4, BiasWeight: 1} // 3
+	input1 := nn.Neuron{Value: 1, Bias: 2, BiasWeight: 1} // 1.5
+	input2 := nn.Neuron{Value: 2, Bias: 4, BiasWeight: 1} // 3
 
-	out1Connections := []*Connection{
-		&Connection{InNeuron: &input1, Weight: 1}, // 1.5
-		&Connection{InNeuron: &input2, Weight: 1}} // 3
-	out2Connections := []*Connection{
-		&Connection{InNeuron: &input1, Weight: 2}, // 3
-		&Connection{InNeuron: &input2, Weight: 1}} // 3
+	out1Connections := []*nn.Connection{
+		&nn.Connection{InNeuron: &input1, Weight: 1}, // 1.5
+		&nn.Connection{InNeuron: &input2, Weight: 1}} // 3
+	out2Connections := []*nn.Connection{
+		&nn.Connection{InNeuron: &input1, Weight: 2}, // 3
+		&nn.Connection{InNeuron: &input2, Weight: 1}} // 3
 
-	output1 := Neuron{Bias: 1, BiasWeight: 2, InConnections: out1Connections} // 1.625
-	output2 := Neuron{Bias: 1, BiasWeight: 1, InConnections: out2Connections} // 1.75
+	output1 := nn.Neuron{Bias: 1, BiasWeight: 2, InConnections: out1Connections} // 1.625
+	output2 := nn.Neuron{Bias: 1, BiasWeight: 1, InConnections: out2Connections} // 1.75
 
-	net := Net{InputNeurons: []*Neuron{&input1, &input2}, OutputNeurons: []*Neuron{&output1, &output2}}
+	net := nn.Net{InputNeurons: []*nn.Neuron{&input1, &input2}, OutputNeurons: []*nn.Neuron{&output1, &output2}}
 
 	// when
 	net.CalculateOutputs()
--- neuralnet/neuron_test.go
+++ neuralnet/neuron_test.go
@@ -1,69 +1,71 @@
-package neuralnet
+package neuralnet_test
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/stretchr/testify/assert"
 
-func TestCalculateValue(t *testing.T) {
-	// given
-	input1 := Neuron{Value: 2}
-	input2 := Neuron{Value: 6}
+// 	nn "github.com/jeyj0/Evogo/neuralnet"
+// )
 
-	inConnections := []*Connection{
-		&Connection{InNeuron: &input1, Weight: 1},
-		&Connection{InNeuron: &input2, Weight: 2}}
+// func TestCalculateValue(t *testing.T) {
+// 	// given
+// 	input1 := nn.Neuron{Value: 2}
+// 	input2 := nn.Neuron{Value: 6}
 
-	outNeuron := Neuron{Bias: 5, BiasWeight: 1, InConnections: inConnections}
+// 	inConnections := []*nn.Connection{
+// 		&nn.Connection{InNeuron: &input1, Weight: 1},
+// 		&nn.Connection{InNeuron: &input2, Weight: 2}}
 
-	// when
-	outNeuron.calculateValue()
+// 	outNeuron := nn.Neuron{Bias: 5, BiasWeight: 1, InConnections: inConnections}
 
-	// then
-	assert.Equal(t, 4.75, outNeuron.Value)
-}
+// 	// when
+// 	outNeuron.calculateValue()
 
-func TestCalculateValueInputNeuron(t *testing.T) {
-	// given
-	neuron := Neuron{Value: 4, Bias: 1, BiasWeight: 2}
+// 	// then
+// 	assert.Equal(t, 4.75, outNeuron.Value)
+// }
 
-	// when
-	neuron.calculateValue()
+// func TestCalculateValueInputNeuron(t *testing.T) {
+// 	// given
+// 	neuron := nn.Neuron{Value: 4, Bias: 1, BiasWeight: 2}
 
-	// then
-	assert.Equal(t, 2.0, neuron.Value)
-}
+// 	// when
+// 	neuron.calculateValue()
 
-func TestCalculateValueRecursive(t *testing.T) {
-	// given
-	input := Neuron{Value: 1}
-	middle := Neuron{Bias: 2, BiasWeight: 4, InConnections: []*Connection{&Connection{InNeuron: &input, Weight: 4}}}
-	output := Neuron{Bias: 5, BiasWeight: 6, InConnections: []*Connection{&Connection{InNeuron: &middle, Weight: 8}}}
+// 	// then
+// 	assert.Equal(t, 2.0, neuron.Value)
+// }
 
-	// (1*4 + 2*4) / (4 + 4) = 1.5 // middle
-	// (1.5*8 + 5*6) / (8 + 6) = 3 // output
+// func TestCalculateValueRecursive(t *testing.T) {
+// 	// given
+// 	input := nn.Neuron{Value: 1}
+// 	middle := nn.Neuron{Bias: 2, BiasWeight: 4, InConnections: []*nn.Connection{&nn.Connection{InNeuron: &input, Weight: 4}}}
+// 	output := nn.Neuron{Bias: 5, BiasWeight: 6, InConnections: []*nn.Connection{&nn.Connection{InNeuron: &middle, Weight: 8}}}
 
-	// when
-	output.calculateValueRecursive()
+// 	// (1*4 + 2*4) / (4 + 4) = 1.5 // middle
+// 	// (1.5*8 + 5*6) / (8 + 6) = 3 // output
 
-	// then
-	assert.Equal(t, 3.0, output.Value)
-}
+// 	// when
+// 	output.calculateValueRecursive()
 
-func TestCalculateValueRecursiveWithInputBias(t *testing.T) {
-	// given
-	input := Neuron{Value: 1, Bias: 2, BiasWeight: 4}
-	middle := Neuron{Bias: 6, BiasWeight: 10, InConnections: []*Connection{&Connection{InNeuron: &input, Weight: 10}}}
-	output := Neuron{Bias: 3, BiasWeight: 14, InConnections: []*Connection{&Connection{InNeuron: &middle, Weight: 16}}}
+// 	// then
+// 	assert.Equal(t, 3.0, output.Value)
+// }
 
-	// input: 1.8
-	// middle: 3.9
-	// output: 3.48
+// func TestCalculateValueRecursiveWithInputBias(t *testing.T) {
+// 	// given
+// 	input := nn.Neuron{Value: 1, Bias: 2, BiasWeight: 4}
+// 	middle := nn.Neuron{Bias: 6, BiasWeight: 10, InConnections: []*nn.Connection{&nn.Connection{InNeuron: &input, Weight: 10}}}
+// 	output := nn.Neuron{Bias: 3, BiasWeight: 14, InConnections: []*nn.Connection{&nn.Connection{InNeuron: &middle, Weight: 16}}}
 
-	// when
-	output.calculateValueRecursive()
+// 	// input: 1.8
+// 	// middle: 3.9
+// 	// output: 3.48
 
-	// then
-	assert.Equal(t, 3.48, output.Value)
-}
+// 	// when
+// 	output.calculateValueRecursive()
+
+// 	// then
+// 	assert.Equal(t, 3.48, output.Value)
+// }
